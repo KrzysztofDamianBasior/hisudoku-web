4cf081b8dd3e943b2283578297e360387717dd00
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2022-08-12 21:01:45 +0200

    feat: add apollo client configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// type AppProps = {
+//   message: string;
+// }
+// const App = ({ message }: AppProps): JSX.Element => <div>{message}</div>;
+// const App: React.FunctionComponent<{ message: string }> = ({ message }) => (
+//   <div>{message}</div>
+// );
+// const [user, setUser] = useState<User | null>(null);
 
 function App() {
   return (
