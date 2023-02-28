d1c5da0356176812ab683b8e6c5852f745d22625
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2023-02-28 16:36:36 +0100

    feat: add a routing transitions animations

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,36 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, Link } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 function App() {
   const location = useLocation();
 
+  const transitionClasses = {
+    enter: "animate__animated",
+    enterActive: "animate__fadeIn",
+    exit: "animate__animated",
+    exitActive: "animate__fadeOut",
+  };
+
   return (
-    <Routes location={location}>
-      <Route path="/" element={<StaticComponent />} />
-      <Route path="/auth" element={<StaticComponent />} />
-      <Route path="/game" element={<StaticComponent />} />
-      <Route path="/profile" element={<StaticComponent />} />
-      <Route path="/sudokus" element={<StaticComponent />} />
-      {/* <Route path="*" element={<StaticComponent />} /> */}
-    </Routes>
+    <TransitionGroup component={null}>
+      <CSSTransition
+        key={location.key}
+        timeout={50000}
+        classNames={transitionClasses}
+      >
+        <Routes location={location}>
+          <Route path="/" element={<StaticComponent />} />
+          <Route path="/auth" element={<StaticComponent />} />
+          <Route path="/game" element={<StaticComponent />} />
+          <Route path="/profile" element={<StaticComponent />} />
+          <Route path="/sudokus" element={<StaticComponent />} />
+          {/* <Route path="*" element={<StaticComponent />} /> */}
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
   );
 }
 
@@ -24,6 +40,8 @@ export default App;
 const StaticComponent = () => {
   return (
     <div className="App">
+      <Link to={"/"}>Home</Link>
+      <Link to={"/auth"}>Auth</Link>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
